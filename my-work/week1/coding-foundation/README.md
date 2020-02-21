a2510276d63d59e7f7a5902ee717f16da2b3f349
Iris-666 <apple@IRISdeMacBook-Air-798.local>
2020-02-21 18:54:36 +0800

    The Cat Generator

---
+++ my-work/week1/coding-foundation/README.md
@@ -0,0 +1,4 @@
+##### How to Use the Cat Generator
+1. Type in a number.
+2. Click "Create Cats" button
+3. Repeat!
---
+++ my-work/week1/coding-foundation/sketch.js
@@ -0,0 +1,11 @@
+let number;
+function createCat(){
+  number = document.getElementById('inputNum').value //get input value
+
+  for (var i = 0; i < number; i++) {
+    var ul = document.createElement('ul'); //create new cats
+    ul.className += "flex-item";
+    document.getElementById('flex').appendChild(ul);//add the cat to parent list
+  }
+
+}
